04ea04a0d46609807737356bcd1015cce8810fa0
pavankumartoragal <121279976+pavankumartoragal@users.noreply.github.com>
2022-12-28 16:01:07 +0530

    b01

---
+++ adsel/src/testngpgm/fb.java
@@ -0,0 +1,13 @@
+package testngpgm;
+
+import org.testng.annotations.Test;
+
+public class fb extends genricFB
+{
+	@Test
+    public void test()
+    {
+	  driver.get("https://www.facebook.com");
+	  
+}
+}
